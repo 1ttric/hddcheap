97c614435d9a53b1a1dcb2575f505adbc10caa82
Will Vesey <willvesey2@gmail.com>
2020-03-25 01:00:05 -0700

    Initial commit

---
+++ api/go.mod
@@ -0,0 +1,10 @@
+module hddcheap
+
+go 1.14
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/gorilla/websocket v1.4.2
+	github.com/sirupsen/logrus v1.5.0
+	github.com/tebeka/selenium v0.9.9
+)
---
+++ web/src/App.js
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+import './App.css';
+import LinearProgress from "@material-ui/core/LinearProgress";
+import MaterialTable from "material-table";
+import {Snackbar} from "@material-ui/core";
+import MuiAlert from '@material-ui/lab/Alert';
+import {ThemeProvider} from "@material-ui/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import Box from "@material-ui/core/Box";
+
+const ws = new WebSocket("ws://127.0.0.1:8080/ws");
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {items: null, lastUpdate: null, updateSnackVisible: false};
+    }
+
+    componentDidMount() {
+        ws.onopen = () => {
+            console.log("Websocket connected");
+        };
+        ws.onmessage = (msg) => {
+            let items = JSON.parse(msg.data);
+            this.setState(state => ({
+                ...state,
+                items: items,
+                lastUpdate: Date.now().toString(),
+                updateSnackVisible: true
+            }))
+        }
+    }
+
+    renderLoading() {
+        return (
+            <LinearProgress/>
+        )
+    }
+
+    renderLoaded() {
+        // Customize some columns
+        let columns = Object.keys(this.state.items[0]).filter(k => (k !== 'tableData')).map(k => {
+                switch (k) {
+                    case 'url':
+                        return {title: k, field: k, render: rowData => <a href={rowData.url}>{rowData.url}</a>};
+                    default:
+                        return {title: k, field: k};
+                }
+            }
+        );
+        columns.push({
+            'title': 'history',
+            'field': 'history',
+            render: rowData => <a href={"https://camelcamelcamel.com/product/" + rowData.asin} target={"_blank"}>
+                <img alt={"Graphed price history from camelcamelcamel"} style={{mixBlendMode: 'multiply'}} src={"https://charts.camelcamelcamel.com/us/" + rowData.asin + "/amazon-new-used.png?legend=1&fo=1&w=900"}/>
+            </a>
+        });
+
+        return (
+            <Box m={2}>
+                <MaterialTable columns={columns}
+                               data={this.state.items}
+                               title={null}
+                />
+                <Snackbar open={this.state.updateSnackVisible}
+                          onClose={() => this.setState(state => ({...state, updateSnackVisible: false}))}
+                          autoHideDuration={2000}>
+                    <MuiAlert severity="info" elevation={6}
+                              variant="filled">{"Updated " + (new Date()).toTimeString()}</MuiAlert>
+                </Snackbar>
+            </Box>
+        );
+    }
+
+
+
+    render() {
+
+        return (
+            <ThemeProvider theme={createMuiTheme()}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6">
+                            hdd.cheap
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <div>{this.state.items === null ? this.renderLoading() : this.renderLoaded()}</div>
+            </ThemeProvider>
+        )
+    }
+}
+
+export default App;
